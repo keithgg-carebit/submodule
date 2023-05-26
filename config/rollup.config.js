29a0e576a90be6084b42e7750d3c2e7fd6eb0520
James Reid <jamesreid3806@gmail.com>
2023-05-26 22:28:53 +0100

    Update config

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,17 +1,54 @@
+// Copyright (c) 2022 James Reid. All rights reserved.
+//
+// This source code file is licensed under the terms of the MIT license, a copy
+// of which may be found in the LICENSE.md file in the root of this repository.  
+// 
+// For a template copy of the license see one of the following 3rd party sites:
+//      * <https://opensource.org/licenses/MIT>
+//      * <https://choosealicense.com/licenses/mit>
+//      * <https://spdx.org/licenses/MIT>
+
+/**
+ * @ignore
+ * @file Rollup config
+ * @author James Reid
+ */
+
+// @ts-check
+
+// @imports-package
 import terser from "@rollup/plugin-terser"
+import postcss from "rollup-plugin-postcss"
 
-const config = {
-    input: "src/plugin/index.js",
-    output: [
-        { file: "dist/bundle.min.cjs", format: "cjs" }
-    ],
-    external: [
-        "path",
-        "jsdoc/env"
-    ],
-    plugins: [
-        terser({ maxWorkers: 6 })
-    ]
-}
+// @body
+// declare rollup config object
+const config = [
+    {
+        input: "src/plugin/index.js",
+        output: [
+            { file: "dist/bundle.min.cjs", format: "cjs" }
+        ],
+        external: [
+            "path",
+            "jsdoc/env"
+        ],
+        plugins: [
+            // @ts-expect-error
+            terser({ maxWorkers: 6 })
+        ]
+    },
+    {
+        input: "src/nested/scripts/docdash/index.js",
+        output: [
+            { file: "dist/docdash.min.js", format: "iife" }
+        ],
+        plugins: [
+            // @ts-expect-error
+            terser({ maxWorkers: 6 }),
+            postcss({ plugins: [] })
+        ]
+    }
+]
 
+// @exports
 export default config
